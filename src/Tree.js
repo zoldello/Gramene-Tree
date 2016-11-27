71a5b9e36cc29454ccc9a70252bfedec2aee5400
zoldello <zoldello@gmail.com>
2016-11-26 20:35:15 -0600

    added foundation for js code

---
+++ src/Tree.js
@@ -0,0 +1,29 @@
+class Tree {
+	constructor(data) {
+		this.data = data;
+		this.flattenData = null;
+	}
+
+	get data () {
+		return this._data
+	}
+
+	get flattenData() {
+	}
+
+	update() {
+	}
+
+	click() {
+	}
+
+	doubleClick() {
+	}
+
+	mouseOver() {
+	}
+
+	mouseIn() {
+	}
+
+}
