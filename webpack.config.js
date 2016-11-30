a9705430ab48818c1eade90ee54d11b7092175c7
zoldello <zoldello@gmail.com>
2016-11-29 21:57:01 -0600

    added some webpack laoders

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,19 @@ module.exports = {
 		filename: "bundle.js"
 	},
 	module: {
+		preLoaders: [
+			{ test: /\.js$/,exclude: /node_modules/, loader: "jshint-loader" },
+		  { css: /\.css$/, test: /\.css/, loader: "style-loader!css-loader" },
+			{ test: /\.html/, loader: 'htmlhint', exclude: /node_modules/ },
+			{ test: /\.js$/, loader: "source-map-loader" }
+		],
 		loaders: [
 			{ test: /\.js$/, exclude:/node_modules/, loader: "babel-loader" },
 			{ test: /\.json$/, exclude: /package.json/,  loader: "json" }
 		]
-	}
+	},
 
+	jshint: {
+		esversion: 6
+	}
 }
